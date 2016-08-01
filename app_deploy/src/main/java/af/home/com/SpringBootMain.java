02bda717efca578fd868b1c3819d9446ca14e046
Константин <exslims1@mail.ru>
2016-08-01 22:32:25 +0400

    Initial commit

---
+++ app_deploy/src/main/java/af/home/com/SpringBootMain.java
@@ -0,0 +1,16 @@
+package af.home.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Exslims
+ * 01.08.2016
+ */
+
+@SpringBootApplication
+public class SpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMain.class,args);
+    }
+}
