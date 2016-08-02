f10e89e64fcd52d7f5434df3061c30d24a79ceb8
Константин <exslims1@mail.ru>
2016-08-02 21:51:23 +0400

    added MongoDB + test

---
+++ app_deploy/src/main/java/af/home/com/AppConfig.java
@@ -0,0 +1,13 @@
+package af.home.com;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Exslims
+ * 02.08.2016
+ */
+@Configuration
+@ComponentScan("af.home.com")
+public class AppConfig {
+}
